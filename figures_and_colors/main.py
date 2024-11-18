e3c79f3358ffefba03e5c78c39e62a668c99b798
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-11-18 18:55:05 +0800

    Added couning of colors in task Figures and colors

--- figures_and_colors/main.py
+++ figures_and_colors/main.py
@@ -9,41 +9,49 @@ from pathlib import Path
 
 
 path = Path(__file__).parent
-im = plt.imread(path/ "balls_and_rects.png")
+im = plt.imread(path/ "balls_and_rects.png")[:500, :500]
+
 binary = im.copy().mean(2)
 binary[binary > 0] = 1
 labeled = label(binary)
 regions = regionprops(labeled)
-
 im_hsv = rgb2hsv(im)
 
-unique_colors = dict()
-colors = []
+
+def isRect(region) -> int:
+    """
+    If region.image is rectangle, returns 1, and 0 if is circle.
+    """
+    y, x = region.image.shape
+    return int(x*y == region.area)
+
+
+colors = defaultdict(lambda: [0, 0])
+unique_colors = defaultdict(lambda: [0, 0])
+
 for i, region in enumerate(regions):
     cy, cx = region.centroid
     color = im_hsv[int(cy), int(cx)][0]
-    colors += [float(color)]
+    is_rect = isRect(region)
+    
+    unique_colors[float(color)][is_rect] += 1
 
     flag = True
-    for clr in unique_colors:
+    for clr in colors:
         if abs(color - clr) < 0.04:
-            unique_colors[float(clr)] += 1
+            colors[float(clr)][is_rect] += 1
             flag = False
             break
     if flag:
-        unique_colors[float(color)] = 1
-    
+        colors[float(color)][is_rect] = 1
+
+
 pprint(unique_colors)
+pprint(colors)
 
-# plt.figure()
-plt.title(f"Colors: {len(unique_colors)}")
-plt.subplot(1, 3, 1)
-plt.imshow(im)
-
-plt.subplot(1, 3, 2)
-plt.imshow(binary)
-
-plt.subplot(1, 3, 3)
-plt.plot(sorted(colors), 'o')
-
-plt.show()
+# plt.subplot(1, 2, 1)
+# plt.imshow(im_hsv[:, :, 0] == 0.6111111044883728)
+# plt.subplot(1, 2, 2)
+# plt.imshow(im_hsv)
+# plt.title(f"Figures: {len(regions)}")
+# plt.show()
